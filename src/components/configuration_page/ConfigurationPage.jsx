e8cfcd0bc60781e45108f867019ab5825b16d9c1
LoggerNice <71228131+LoggerNice@users.noreply.github.com>
2022-08-13 23:27:52 +0400

    Json

--- src/components/configuration_page/ConfigurationPage.jsx
+++ src/components/configuration_page/ConfigurationPage.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import ConfigurationItem from './configuration_item/ConfigurationItem'
 import s from './Page.module.css'
 
+import data from './Assembly.json'
+
 const ConfigurationPage = () => {
   return (
     <div>
       <p className={s.header}>Собери свою мечту</p>
-      <ConfigurationItem/>
+      <ConfigurationItem img={data[0].img} type={data[0].type} title={data[0].title} description={data[0].description} price={'$' + data[0].price}/>
     </div>
   )
 }
